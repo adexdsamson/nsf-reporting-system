80999f8d4a68eb3fead865f770d0f8e0cc13130e
Adediran Adeola <35856417+adexdsamson@users.noreply.github.com>
2025-03-02 14:09:54 +0100

    Update client.tsx

--- src/lib/graphql/client.tsx
+++ src/lib/graphql/client.tsx
@@ -11,7 +11,7 @@ const httpLink = createHttpLink({
 });
 
 const wsLink = new WebSocketLink({
-  uri : `ws://${baseUrl}/graphql`,
+  uri : `wss://${baseUrl}/graphql`,
 });
 
 const splitLink = split(
